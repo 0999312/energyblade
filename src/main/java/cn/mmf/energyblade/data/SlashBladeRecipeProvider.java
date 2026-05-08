099feaf5499a1bffbbef74abdcededd9b96da16b
unknown <mr09991999@outlook.com>
2026-05-08 20:26:42 +0800

    feat(05-datagen-and-resources): rewrite recipe provider for RecipeOutput + NeoForge 1.21.1 API

--- src/main/java/cn/mmf/energyblade/data/SlashBladeRecipeProvider.java
+++ src/main/java/cn/mmf/energyblade/data/SlashBladeRecipeProvider.java
@@ -1,28 +1,29 @@
 package cn.mmf.energyblade.data;
 
-import java.util.function.Consumer;
+import java.util.concurrent.CompletableFuture;
 
 import mods.flammpfeil.slashblade.init.SBItems;
 import mods.flammpfeil.slashblade.recipe.RequestDefinition;
 import mods.flammpfeil.slashblade.recipe.SlashBladeIngredient;
 import mods.flammpfeil.slashblade.recipe.SlashBladeShapedRecipeBuilder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraftforge.common.Tags;
-import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 
 public class SlashBladeRecipeProvider extends RecipeProvider implements IConditionBuilder {
 
-    public SlashBladeRecipeProvider(PackOutput output) {
-        super(output);
+    public SlashBladeRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries);
     }
 
-	@Override
-	public void buildRecipes(Consumer<FinishedRecipe> consumer) {
-		SlashBladeShapedRecipeBuilder.shaped(BuiltInSlashBladeRegistry.HF_BLADE.location())
-		.pattern("SLJ")
+    @Override
+    protected void buildRecipes(RecipeOutput output) {
+        SlashBladeShapedRecipeBuilder.shaped(BuiltInSlashBladeRegistry.HF_BLADE.location())
+        .pattern("SLJ")
         .pattern("LBL")
         .pattern("JLS")
         .define('B',
@@ -31,8 +32,8 @@ public class SlashBladeRecipeProvider extends RecipeProvider implements IConditi
         .define('S', Ingredient.of(SBItems.proudsoul_sphere))
         .define('J', Ingredient.of(Tags.Items.STORAGE_BLOCKS_REDSTONE))
         .define('L', Ingredient.of(Tags.Items.STORAGE_BLOCKS_IRON))
-        .unlockedBy(getHasName(SBItems.slashblade), has(SBItems.slashblade)).save(consumer);
+        .unlockedBy(getHasName(SBItems.slashblade), has(SBItems.slashblade)).save(output);
 
-	}
+    }
 
 }
